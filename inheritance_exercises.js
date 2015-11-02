45827cc17bfe9cf075f4df054a989f8ac2e355b6
William&Ursula <appacademy@Apps-Mac-mini-31.local>
2015-11-02 11:42:49 -0500

    Complete inheritance exercises.

--- inheritance_exercises.js
+++ inheritance_exercises.js
@@ -1,3 +1,10 @@
 Function.prototype.inherits = function(parent) {
-  
-}
+  var Surrogate = function() {};
+
+  Surrogate.prototype = parent.prototype;
+
+  this.prototype = new Surrogate();
+
+  this.prototype.constructor = this;
+
+};
